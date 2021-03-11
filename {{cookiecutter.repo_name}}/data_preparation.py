94c5e5d9577acb2146b6746dadfc0f501425c62d
Teddy Groves <groves.teddy@gmail.com>
2021-03-11 12:38:35 +0100

    Split data preparation into script and source

---
+++ {{cookiecutter.repo_name}}/data_preparation.py
@@ -0,0 +1,26 @@
+"""Provides a function prepare_data."""
+
+import pandas as pd
+
+from util import make_columns_lower_case
+
+RENAMING_DICT = {"yButIThoughtIdAddSomeLetters": "y"}
+COLS_THAT_MUST_BE_NON_NULL = ["y"]
+
+
+def prepare_data(raw_data: pd.DataFrame) -> pd.DataFrame:
+    """Takes in raw data, returns prepared data.
+
+    :param raw_data: pd.DataFrame of raw data
+    ::
+    """
+    out = (
+        raw_data.copy()
+        .rename(columns=RENAMING_DICT)
+        .pipe(make_columns_lower_case)
+        .dropna(subset=COLS_THAT_MUST_BE_NON_NULL)
+        .drop_duplicates()
+        .reset_index(drop=True)
+    )
+    out["x1:x2"] = out["x1"] * out["x2"]
+    return out
--- {{cookiecutter.repo_name}}/prepare_data.py
+++ {{cookiecutter.repo_name}}/prepare_data.py
@@ -1,10 +1,8 @@
 """Prepare the data at RAW_DATA_CSV and save it to PREPARED_DATA_CSV."""
 
 import os
-from typing import List, Dict
 import pandas as pd
-
-from util import make_columns_lower_case
+from data_preparation import prepare_data
 
 # Where to find raw data and where to save prepared data: probably don't edit!
 RAW_DATA_DIR = os.path.join("data", "raw")
@@ -15,31 +13,6 @@ PREPARED_DATA_DIR = os.path.join("data", "prepared")
 RAW_DATA_CSV = "raw_measurements.csv"
 PREPARED_DATA_CSV = "data_prepared.csv"
 
-RENAMING_DICT = {"yButIThoughtIdAddSomeLetters": "y"}
-COLS_THAT_MUST_BE_NON_NULL = ["y"]
-
-
-def prepare_data(
-    raw_data: pd.DataFrame,
-    renaming_dict: Dict[str, str],
-    cols_that_must_be_non_null: List[str],
-) -> pd.DataFrame:
-    """Takes in raw data, returns prepared data.
-
-    :param raw_data: pd.DataFrame of raw data
-    ::
-    """
-    out = (
-        raw_data.copy()
-        .rename(columns=renaming_dict)
-        .pipe(make_columns_lower_case)
-        .dropna(subset=cols_that_must_be_non_null)
-        .drop_duplicates()
-        .reset_index(drop=True)
-    )
-    out["x1:x2"] = out["x1"] * out["x2"]
-    return out
-
 
 def main():
     """Run the script."""
@@ -47,7 +20,7 @@ def main():
     prepared_data_csv = os.path.join(PREPARED_DATA_DIR, PREPARED_DATA_CSV)
     print(f"Reading raw data from {raw_data_csv}")
     raw = pd.read_csv(raw_data_csv)
-    out = prepare_data(raw, RENAMING_DICT, COLS_THAT_MUST_BE_NON_NULL)
+    out = prepare_data(raw)
     print(f"Writing prepared data to {prepared_data_csv}")
     out.to_csv(prepared_data_csv)
 
