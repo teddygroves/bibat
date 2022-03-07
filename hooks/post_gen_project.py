78b61f27ff9caf84935a9473eff51ead99fa9d01
Teddy Groves <groves.teddy@gmail.com>
2022-03-07 12:24:57 +0100

    Add optional .github directory

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,7 @@ from shutil import rmtree
 REMOVE_PATHS = [
     '{% if cookiecutter.create_writing_directory != "y" %} writing {% endif %}',
     '{% if cookiecutter.create_tests_directory != "y" %} tests {% endif %}',
+    '{% if cookiecutter.create_dotgithub_directory != "y" %} .github {% endif %}',
 ]
 
 for path in REMOVE_PATHS:
