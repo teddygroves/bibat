61184f4f18432af257f77df19601f83577bcc60d
teddygroves <groves.teddy@gmail.com>
2023-12-15 13:39:54 +0100

    Save prepared data in a single file
    This change saves each `PreparedData` object as a single json file,
    avoiding the need for complicated saving and loading operations.

--- bibat/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/data_preparation.py
+++ bibat/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/data_preparation.py
@@ -1,61 +1,28 @@
-"""Provides functions prepare_data_x.
+"""Provides function `prepare_data` and runs it.
 
-These functions should take in a dataframe of measurements and return a
-PreparedData object.
+This function should run some other functions with names `prepare_data_x`, which
+each take in a dataframe of measurements and return a PreparedData object.
 
 """
 import json
 import os
+from io import StringIO
+from pathlib import Path
+from typing import Any
 
 import numpy as np
 import pandas as pd
 import pandera as pa
+from pandas._libs.lib import item_from_zerodim
 from pandera.typing import DataFrame, Series
-from pydantic import BaseModel
+from pandera.typing.common import DataFrameBase
+from pydantic import BaseModel, field_serializer, field_validator
 from {{cookiecutter.repo_name}} import util
 
-NAME_FILE = "name.txt"
-COORDS_FILE = "coords.json"
-MEASUREMENTS_FILE = "measurements.csv"
-NEW_COLNAMES = {"yButIThoughtIdAddSomeLetters": "y"}
-DROPNA_COLS = ["y"]
-N_CV_FOLDS = 10
-DIMS = {
-    "b": ["covariate"],
-    "y": ["observation"],
-    "yrep": ["observation"],
-    "llik": ["observation"],
-}
-
-HERE = os.path.dirname(__file__)
-DATA_DIR = os.path.join(HERE, "..", "data")
-RAW_DIR = os.path.join(DATA_DIR, "raw")
-PREPARED_DIR = os.path.join(DATA_DIR, "prepared")
-RAW_DATA_FILES = {
-    "raw_measurements": os.path.join(RAW_DIR, "raw_measurements.csv"),
-}
-
-
-def prepare_data():
-    """Run main function."""
-    print("Reading raw data...")
-    raw_data = {
-        k: pd.read_csv(v, index_col=None) for k, v in RAW_DATA_FILES.items()
-    }
-    data_preparation_functions_to_run = [
-        prepare_data_interaction,
-        prepare_data_no_interaction,
-        prepare_data_fake_interaction
-    ]
-    print("Preparing data...")
-    for dpf in data_preparation_functions_to_run:
-        print(f"Running data preparation function {dpf.__name__}...")
-        prepared_data = dpf(raw_data["raw_measurements"])
-        output_dir = os.path.join(PREPARED_DIR, prepared_data.name)
-        print(f"\twriting files to {output_dir}")
-        if not os.path.exists(PREPARED_DIR):
-            os.mkdir(PREPARED_DIR)
-        write_prepared_data(prepared_data, output_dir)
+HERE = Path(__file__).parent
+RAW_DIR = HERE /  ".." / "data" / "raw"
+PREPARED_DIR = HERE / ".." / "data" / "prepared"
+RAW_DATA_FILES = {"measurements": RAW_DIR / "raw_measurements.csv"}
 
 
 class MeasurementsDF(pa.SchemaModel):
@@ -75,32 +42,29 @@ class PreparedData(BaseModel, arbitrary_types_allowed=True):
 
     name: str
     coords: util.CoordDict
-    measurements: DataFrame[MeasurementsDF]
+    measurements: Any
+
+    @field_validator("measurements")
+    def validate_measurements(
+        cls, v: Any
+    ) -> DataFrameBase[MeasurementsDF]:
+        if isinstance(v, str):
+            v = pd.read_json(StringIO(v))
+        return MeasurementsDF.validate(v)
+
+    @field_serializer("measurements")
+    def serialize_measurements(
+        self, measurements: DataFrame[MeasurementsDF], _info
+    ):
+        return measurements.to_json()
 
 
-def load_prepared_data(directory: str) -> PreparedData:
-    """Load prepared data from files in directory."""
-    with open(os.path.join(directory, COORDS_FILE), "r") as f:
-        coords = json.load(f)
-    with open(os.path.join(directory, NAME_FILE), "r") as f:
-        name = f.read()
-    measurements = pd.read_csv(os.path.join(directory, MEASUREMENTS_FILE))
-    return PreparedData(
-        name=name,
-        coords=coords,
-        measurements=DataFrame[MeasurementsDF](measurements),
-    )
+def load_prepared_data(path_to_data: str) -> PreparedData:
+    """Load a dataset."""
+    with open(path_to_data) as f:
+        raw = json.load(f)
+    return PreparedData(**raw)
 
-
-def write_prepared_data(prepped: PreparedData, directory):
-    """Write prepared data files to a directory."""
-    if not os.path.exists(directory):
-        os.mkdir(directory)
-        prepped.measurements.to_csv(os.path.join(directory, MEASUREMENTS_FILE))
-    with open(os.path.join(directory, COORDS_FILE), "w") as f:
-        json.dump(prepped.coords, f)
-    with open(os.path.join(directory, NAME_FILE), "w") as f:
-        f.write(prepped.name)
 
 def prepare_data_interaction(measurements_raw: pd.DataFrame) -> PreparedData:
     """Prepare data with an interaction column."""
@@ -165,12 +129,40 @@ def process_measurements(measurements: pd.DataFrame) -> pd.DataFrame:
     functions for different prepare_data functions
 
     """
+    dropna_cols = ["y"]
+    new_colnames = {"yButIThoughtIdAddSomeLetters": "y"}
     out = (
-        measurements.rename(columns=NEW_COLNAMES)
+        measurements.rename(columns=new_colnames)
         .pipe(util.make_columns_lower_case)
-        .dropna(subset=DROPNA_COLS, axis=0)
+        .dropna(subset=dropna_cols, axis=0)
     ).copy()
     for col in ["x1", "x2", "y"]:
         out[col] = out[col].astype(float)
     out["x1:x2"] = out["x1"] * out["x2"]
     return out
+
+
+def prepare_data():
+    """Run main function."""
+    print("Reading raw data...")
+    raw_data = {
+        k: pd.read_csv(v, index_col=None) for k, v in RAW_DATA_FILES.items()
+    }
+    print("Preparing data...")
+    for prepare_data_func in  [
+        prepare_data_interaction,
+        prepare_data_no_interaction,
+        prepare_data_fake_interaction
+    ]:
+        print(f"Running data preparation function {prepare_data_func.__name__}...")
+        prepared_data = prepare_data_func(raw_data["measurements"])
+        output_file = os.path.join(PREPARED_DIR, prepared_data.name + ".json")
+        print(f"\twriting prepared_data to {output_file}")
+        if not os.path.exists(PREPARED_DIR):
+            os.mkdir(PREPARED_DIR)
+        with open(output_file, "w") as f:
+            f.write(prepared_data.model_dump_json())
+
+
+if __name__ == "__main__":
+    prepare_data()
